18bc4b70bee0249289acb7aa6941f0af569c2c7b
CristianS08 <cristians0898@hotmail.com>
2024-06-09 19:50:27 -0300

    add tests

--- src/main/java/com/gruposeis/testing/ProductoService.java
+++ src/main/java/com/gruposeis/testing/ProductoService.java
@@ -25,6 +25,16 @@ public class ProductoService {
         return numero1 + numero2;
     }
 
+    public void actualizarStock(Producto producto, int cantidad) {
+        producto.setStock(cantidad);
+    }
 
+    public void marcarProductoComoBaja(Producto producto) {
+        producto.marcarComoBaja();
+    }
+
+    public Optional<Integer> consultarStock(Producto producto) {
+        return Optional.ofNullable(producto.getStock());
+    }
 
 }
--- src/test/java/com/gruposeis/testing/ProductoServiceTest.java
+++ src/test/java/com/gruposeis/testing/ProductoServiceTest.java
@@ -1,9 +1,15 @@
 package com.gruposeis.testing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Optional;
 
 @SpringBootTest
 public class ProductoServiceTest {
@@ -25,5 +31,30 @@ public class ProductoServiceTest {
 
 
 
+    }
+
+    @Test
+    public void testActualizarStock() {
+        ProductoService productoService = new ProductoService();
+        Producto producto = new Producto(1L, "Producto 1", 10);
+        productoService.actualizarStock(producto, 20);
+        assertEquals(20, producto.getStock());
+    }
+
+    @Test
+    public void testConsultarStock() {
+        ProductoService productoService = new ProductoService();
+        Producto producto = new Producto(1L, "Producto 1", 10);
+        Optional<Integer> stock = productoService.consultarStock(producto);
+        assertTrue(stock.isPresent());
+        assertEquals(10, stock.get().intValue());
+    }
+
+    @Test
+    public void testMarcarProductoComoBaja() {
+        ProductoService productoService = new ProductoService();
+        Producto producto = new Producto(1L, "Producto 1", 10);
+        productoService.marcarProductoComoBaja(producto);
+        assertEquals(LocalDate.now(), producto.getFechaHoraBaja());
     }
 }
